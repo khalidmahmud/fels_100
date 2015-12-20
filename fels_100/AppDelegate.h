a3ca267133eee935d3fb1f759d09a41acab4496d
khalidmahmud <abu.khalid.mubashshir.mahmud@framgia.com>
2015-12-20 21:38:41 +0600

    48775_Project_Skeleton

---
+++ fels_100/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fels_100
+//
+//  Created by Abu Khalid on 12/17/15.
+//  Copyright © 2015 Abu Khalid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fels_100/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fels_100
+//
+//  Created by Abu Khalid on 12/17/15.
+//  Copyright © 2015 Abu Khalid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
