d9fb7b3c9b6995be476b255230eeb0d085259e16
dina28 <kazi.sharmin.dina@framgia.com>
2015-12-22 11:35:40 +0600

    48795_task

---
+++ fels_100/categoriesTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  categoriesTableViewCell.h
+//  fels_100
+//
+//  Created by Kazi Sharmin Dina on 12/22/15.
+//  Copyright © 2015 Abu Khalid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface categoriesTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *categoriesImage;
+@property (weak, nonatomic) IBOutlet UILabel *categoriesTypeLb;
+@property (weak, nonatomic) IBOutlet UILabel *categoriesDetailsLb;
+
+@end
---
+++ fels_100/categoriesTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  categoriesTableViewCell.m
+//  fels_100
+//
+//  Created by Kazi Sharmin Dina on 12/22/15.
+//  Copyright © 2015 Abu Khalid. All rights reserved.
+//
+
+#import "categoriesTableViewCell.h"
+
+@implementation categoriesTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
